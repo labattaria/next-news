85245632f37d02f6075fca203dd233cab00153d5
labattaria <hallucinate322@gmail.com>
2025-08-04 17:44:04 +0300

    add notFound() fallback for invalid news slug in news detail page

--- app/news/[newsSlug]/page.js
+++ app/news/[newsSlug]/page.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
   const newsId = params.newsSlug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsId);
+
+  if (!newsItem) {
+    notFound();
+  }
 
   return (
     <article className="news-article">

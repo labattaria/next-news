9c4dc164e7adb39ed5c0f32ed8230a8d6740e426
labattaria <hallucinate322@gmail.com>
2025-08-05 23:02:37 +0300

    move intersection page into @modal to show image in modal

---
+++ app/news/[newsSlug]/@modal/(.)image/page.js
@@ -0,0 +1,28 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+
+export default function InterceptedImagePage({ params }) {
+  const newsItemSlug = params.newsSlug;
+  const newsItem = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === newsItemSlug
+  );
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img
+            src={`/images/news/${newsItem.image}`}
+            alt={newsItem.title}
+            width="736"
+          />
+        </div>
+      </dialog>
+    </>
+  );
+}

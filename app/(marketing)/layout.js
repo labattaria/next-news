57b76143406141df9af502e3a82d324185623475
labattaria <hallucinate322@gmail.com>
2025-08-08 01:16:09 +0300

    update root layout metadata

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,8 +1,9 @@
 import "../globals.css";
 
 export const metadata = {
-  title: "Next.js Page Routing & Rendering",
-  description: "Learn how to route to different pages.",
+  title: "Your daily NextNews",
+  description:
+    "Your trusted source for the latest updates, exclusive reports, and stories that matter.",
 };
 
 export default function RootLayout({ children }) {

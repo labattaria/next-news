bd3372e6494984ba51e2d2929bdf05c30c92cdc9
labattaria <hallucinate322@gmail.com>
2025-08-07 23:47:30 +0300

    refactor: convert to async server components

--- app/(content)/news/[newsSlug]/page.js
+++ app/(content)/news/[newsSlug]/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import { getNewsItem } from "@/lib/news";
 
-export default function NewsDetailPage({ params }) {
-  const newsId = params.newsSlug;
-  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsId);
+export default async function NewsDetailPage({ params }) {
+  const newsSlug = params.newsSlug;
+  const newsItem = await getNewsItem(newsSlug);
 
   if (!newsItem) {
     notFound();
@@ -17,7 +17,9 @@ export default function NewsDetailPage({ params }) {
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
         </Link>
         <h1>{newsItem.title}</h1>
-        <time dateTime={newsItem.date}>{newsItem.date}</time>
+        <time dateTime={newsItem.date.toISOString()}>
+          {newsItem.date.toDateString()}
+        </time>
       </header>
       <p>{newsItem.content}</p>
     </article>
